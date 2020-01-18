5b408158328ee8833b9e396266a3409c11ce1ad0
Victor Rabelo <victorabelo1697@gmail.com>
2020-01-18 00:51:02 -0300

    Funcionalidades avançadas

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const http = require('http');
+
 const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect(
   'mongodb+srv://omnistack:omnistack@cluster0-rixcj.mongodb.net/week10?retryWrites=true&w=majority',
@@ -27,4 +33,4 @@ app.use(routes);
 
 // MongoDB (Não-relacional)
 
-app.listen(3333);
+server.listen(3333);
